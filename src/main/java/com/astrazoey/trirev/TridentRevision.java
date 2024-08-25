bdd29a34a9484775241018b4e297ce99aecd0df1
ZoeyTheEgoist <jragon.alt1@gmail.com>
2024-08-25 14:13:31 +0800

    updated to 1.20

--- src/main/java/com/astrazoey/trirev/TridentRevision.java
+++ src/main/java/com/astrazoey/trirev/TridentRevision.java
@@ -2,11 +2,10 @@ package com.astrazoey.trirev;
 
 import com.astrazoey.trirev.registry.TridentItems;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
-import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.minecraft.entity.EntityType;
+import net.minecraft.loot.LootPool;
 import net.minecraft.loot.entry.ItemEntry;
-import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.util.Identifier;
 
 public class TridentRevision implements ModInitializer {
@@ -21,14 +20,15 @@ public class TridentRevision implements ModInitializer {
     public void onInitialize() {
         TridentItems.registerItems();
 
-        LootTableLoadingCallback.EVENT.register((resourceManager, lootManager, id, table, setter) -> {
-            if (ELDER_GUARDIAN_LOOT_TABLE_ID.equals(id)) {
-                FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
-                        .rolls(ConstantLootNumberProvider.create(1))
+        LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+            if (source.isBuiltin() && ELDER_GUARDIAN_LOOT_TABLE_ID.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
                         .with(ItemEntry.builder(TridentItems.ELDER_FRAGMENT));
-                table.pool(poolBuilder);
+
+                tableBuilder.pool(poolBuilder);
             }
         });
+
 
     }
 
--- src/main/java/com/astrazoey/trirev/registry/TridentItems.java
+++ src/main/java/com/astrazoey/trirev/registry/TridentItems.java
@@ -1,16 +1,24 @@
 package com.astrazoey.trirev.registry;
 
 import com.astrazoey.trirev.TridentRevision;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 
 public class TridentItems {
 
-    public static final Item ELDER_FRAGMENT = new Item(new Item.Settings().group(ItemGroup.MISC));
+    public static final Item ELDER_FRAGMENT = new Item(new Item.Settings());
+    private static void addItemsToIngredientsTabItemGroup(FabricItemGroupEntries entries) {
+        entries.add(ELDER_FRAGMENT);
+    }
 
     public static void registerItems() {
-        Registry.register(Registry.ITEM, new Identifier(TridentRevision.MOD_ID, "elder_fragment"), ELDER_FRAGMENT);
+        Registry.register(Registries.ITEM, new Identifier(TridentRevision.MOD_ID, "elder_fragment"), ELDER_FRAGMENT);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(TridentItems::addItemsToIngredientsTabItemGroup);
     }
 }
